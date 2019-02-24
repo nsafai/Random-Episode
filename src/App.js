d874caadbdcadc39f4eaa72438417225c79fbbc5
Nicolai Safai <nicolaisafai@gmail.com>
2019-02-23 23:56:37 -0800

    getting json data from moviedb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable no-undef */
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 import './App.css';
+// const fetch = require('node-fetch');
 import NavBar from './NavBar/NavBar';
 import DisplayEpisode from './Episode/DisplayEpisode';
 
 const seriesName = 'Friends';
-const MOVIEDB_API_KEY = process.env.REACT_APP_MOVIEDB_KEY;
-console.log(MOVIEDB_API_KEY);
-
-// const seasonQuery = `https://api.themoviedb.org/3/tv/1668?api_key=${MOVIEDB_API_KEY}&language=en-US`;
-// const episodeQuery = `https://api.themoviedb.org/3/tv/1668/season/1/episode/1?api_key=${MOVIEDB_API_KEY}&language=en-US`;
 
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      // seriesData: null, // used to hold data from OMDB API
-    };
   }
 
   render() {
--- src/Episode/DisplayEpisode.js
+++ src/Episode/DisplayEpisode.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable object-curly-newline */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { Component } from 'react';
 import './DisplayEpisode.css';
 import EpisodeImage from './EpisodeImage/EpisodeImage';
 import SeriesTitle from './SeriesTitle/SeriesTitle';
@@ -18,24 +20,58 @@ const randomEpNum = RandomEpNumGenerator(baseNetflixID, totalNumEpisodes);
 const netflixUrl = 'https://www.netflix.com/watch/';
 const randomEpUrl = netflixUrl.concat(randomEpNum);
 
-const DisplayEpisode = ({
-  name,
-  epName = 'The One Where Ross Hugs Rachel',
-  epSummary = `Monica and Chandler try to tell Rachel and Joey that they're moving in together. 
-  Phoebe thinks that Ross didn't get the annulment because he still loves Rachel.`,
-}) => (
-  <div id="episode-container">
-    <SeriesTitle name={name} />
-    <div>
-      <p className="helper-text">Your random episode is:</p>
-      <EpisodeImage imgUrl="https://m.media-amazon.com/images/M/MV5BMjAzMDI0MjY4MV5BMl5BanBnXkFtZTgwNTg2MzE1NTE@._V1_.jpg" />
-      <EpisodeDetails epName={epName} epSummary={epSummary} />
-    </div>
-    <div className="buttons-bar">
-      <Button url={randomEpUrl} text="Watch on Netflix" color="#c52525" />
-      <Button url="/" text="Another Episode" />
-    </div>
-  </div>
-);
+// MOVIE_DB_SETUP
+const MOVIEDB_API_KEY = process.env.REACT_APP_MOVIEDB_KEY;
+
+class DisplayEpisode extends Component {
+  constructor(props) {
+    super(props);
+    this.name = 'Friends';
+    this.epName = 'The One Where Ross Hugs Rachel';
+    this.epSummary = `Monica and Chandler try to tell Rachel and Joey that they're moving in together. 
+    Phoebe thinks that Ross didn't get the annulment because he still loves Rachel.`;
+    // this.seasonQuery = `https://api.themoviedb.org/3/tv/1668?api_key=${MOVIEDB_API_KEY}&language=en-US`;
+    // this.episodeQuery = `https://api.themoviedb.org/3/tv/1668/season/1/episode/1?api_key=${MOVIEDB_API_KEY}&language=en-US`;
+
+    this.state = {
+      seriesData: {}, // used to hold data from OMDB API
+    };
+  }
+
+  componentWillMount() {
+    fetch(`https://api.themoviedb.org/3/tv/1668?api_key=${MOVIEDB_API_KEY}&language=en-US`)
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({ seriesData: json });
+        console.log(json);
+        // return seriesData;
+      })
+      .catch((err) => {
+        this.setState({ seriesData: {} });
+        console.log('-- Error fetching --');
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div id="episode-container">
+        <div>
+          <SeriesTitle name={this.name} />
+          <p>{this.state.seriesData.number_of_episodes}</p>
+        </div>
+        <div>
+          {/* <p className="helper-text">Your random episode is:</p> */}
+          <EpisodeImage imgUrl="https://m.media-amazon.com/images/M/MV5BMjAzMDI0MjY4MV5BMl5BanBnXkFtZTgwNTg2MzE1NTE@._V1_.jpg" />
+          <EpisodeDetails epName={this.epName} epSummary={this.epSummary} />
+        </div>
+        <div className="buttons-bar">
+          <Button url={randomEpUrl} text="Watch on Netflix" color="#c52525" />
+          <Button url="/" text="Another Episode" />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default DisplayEpisode;
