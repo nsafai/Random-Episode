a45d032664d4e2a9ba1b25423e5b2a9ece56b9c2
Nicolai Safai <nicolaisafai@gmail.com>
2019-02-24 17:20:16 -0800

    preparing to ship to heroku

---
+++ build/precache-manifest.e4e2606c2d533ba4a46d271e3011ff1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2abdd2d7677489523b19",
+    "url": "/static/js/main.2abdd2d7.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2abdd2d7677489523b19",
+    "url": "/static/css/main.61445498.chunk.css"
+  },
+  {
+    "revision": "9d6c4bd04e12276ea17b248a3b8b22ad",
+    "url": "/index.html"
+  }
+];
